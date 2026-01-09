fa4e5554b08898f4d2780d952978f70d0bceec0c
prince <nicksingh914@gmail.com>
2026-01-09 18:06:48 +0530

    more function

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-document.querySelectorAll('.add-task').forEach((button, index) => {
-    const columns = ['todo', 'doing', 'done'];
-    const columnId = columns[index];
-    
-    button.addEventListener('click', () => {
-        const input = document.getElementById(`${columnId}-input`);
-        const tasksContainer = document.getElementById(`${columnId}-tasks`);
-        
-        if (input.value.trim() === '') return;
-        
-        const taskEl = document.createElement('div');
-        taskEl.className = 'task';
-        taskEl.textContent = input.value;
-        taskEl.draggable = true;
-        
-        tasksContainer.appendChild(taskEl);
-        input.value = '';
-    });
-});
+let draggedCard=null;
+function addTask(columnId){
+    const input = document.getElementById(`${columnId.toLowerCase()}-input`);
+    const taskText = input.value.trim();
+
+    if (taskText===""){
+        return;
+    }
+
+
+    const taskElement = createTaskElement(taskText);
+    document.getElementById(`${columnId.toLowerCase()}-tasks`).appendChild(taskElement);
+    input.value="";
+}
+
+function createTaskElement(taskText){
+    const taskElement = document.createElement("div");
+    taskElement.textContent=taskText;
+    taskElement.classList.add("card");
+   // taskElement.setAttribute("draggable",true);
+    taskElement.draggable=true;
+    taskElement.addEventListener("dragstart", dragStart);
+    taskElement.addEventListener("dragend",dragEnd);
+    return taskElement;
+
+}
+
+function dragStart(){
+    this.classList.add("dragging");
+    draggedCard=this;
+
+}
+
+function dragEnd(){
+    this.classList.remove("dragging");
+    draggedCard = null;
+}
+const column = document.querySelectorAll(".column .tasks" );
+column.forEach((column =>{
+    column.addEventListener("dragover", dragOver);
+}));
+
+
+function dragOver(event){
+    event.preventDefault();
+    this.appendChild(draggedCard);
+}
+
+
+
